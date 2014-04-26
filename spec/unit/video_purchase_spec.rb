b53b49fe499f91fd7d8e4e410d6d5a5a076ede0d
Jack Franklin <jack@jackfranklin.net>
2014-04-26 15:13:55 +0100

    tidy up video purchase specs

--- spec/unit/video_purchase_spec.rb
+++ spec/unit/video_purchase_spec.rb
@@ -16,8 +16,20 @@ describe VideoPurchase do
     ).create
   end
 
-  it "creates a new stripe customer" do
+  def stub_charge_success
     Stripe::Charge.stubs(:create).returns(Struct.new(:paid, :id).new(true, 4567))
+  end
+
+  def stub_charge_fail
+    Stripe::Charge.stubs(:create).returns(Struct.new(:paid, :id).new(false, 4567))
+  end
+
+  def stub_customer
+    Stripe::Customer.stubs(:create).returns(Struct.new(:id).new(1234))
+  end
+
+  it "creates a new stripe customer" do
+    stub_charge_success
     Stripe::Customer.expects(:create).with({
       email: 'test',
       card: 'test'
@@ -33,23 +45,20 @@ describe VideoPurchase do
       description: "Purchase of '#{video.title} from Devcast.in'"
     }).returns(Struct.new(:paid, :id).new(true, 4567))
 
-    Stripe::Customer.stubs(:create).with({
-      email: 'test',
-      card: 'test'
-    }).returns(Struct.new(:id).new(1234))
+    stub_customer
     create_video
   end
 
   it "creates a purchase if payment successful" do
-    Stripe::Customer.stubs(:create).returns(Struct.new(:id).new(1234))
-    Stripe::Charge.stubs(:create).returns(Struct.new(:paid, :id).new(true, 4567))
+    stub_customer
+    stub_charge_success
     expect(create_video).to be_a(Purchase)
     expect(create_video.video).to eq(video)
   end
 
   it "returns false if payment failed" do
-    Stripe::Customer.stubs(:create).returns(Struct.new(:id).new(1234))
-    Stripe::Charge.stubs(:create).returns(Struct.new(:paid, :id).new(false, 4567))
+    stub_customer
+    stub_charge_fail
     expect(create_video).to be_false
   end
 end
